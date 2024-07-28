89e1b809d8264b6498ca7a466e82353eafbc8eef
Jordan <inojld@proton.me>
2024-07-28 12:25:40 -0400

    Create calibration.cpp

---
+++ calibration.cpp
@@ -0,0 +1,20 @@
+// Use this code to test for your thresholds 
+// C++
+
+#include <Arduino.h>
+
+int sensor = A0;
+int sensor_value = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Uno has started");
+  Serial.println("");
+}
+
+void loop() {
+  sensor_value = analogRead(sensor);
+  Serial.println(sensor_value);
+  delay(2000);
+}
+
